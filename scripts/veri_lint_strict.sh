25d2af7932366a46fd8820eb2ec93d7f971ea37e
Guillem <cabo.guillem@gmail.com>
2019-10-23 17:04:35 +0200

    updated makefile and lint scripts
    added strict version of linting that fails after first error

--- scripts/veri_lint_strict.sh
+++ scripts/veri_lint_strict.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#This script will lint all the verilog and sv files with verilator
-#An artifact with the errors is generated
-artifact="/tmp/artifact_lint.log"
+#This script will lint all the verilog and sv files with verilator til an error
+# is found. No artefacts are made.
+
 #find paths of directories that are called rtl and save them in the format of
 #verilator. path -I"other_paths"
 include_dirs=""
@@ -17,30 +17,17 @@ while read file; do
     rtl_files=$rtl_files$file$'\n'
 done < <(grep -r -l -i --include \*.v --include \*.sv ./ --exclude \tb_* --exclude \wpi_* )
 
-#Keep this only for debug pourpouses, can be removed in the future. TODO
-#echo "paths of include_dirs"
-#echo "-----------------"
-#echo $include_dirs
-#echo "paths of rtl_files"
-#echo "-----------------"
-#echo -e $rtl_files
-
-
 echo "Verilator lint only"
 echo "-----------------"
-rm $artifact
 while read p; do
   #include all the founded rtl folders as includes in case you have dependences 
   #grep for warnings and errors and save it on a variable. Notice that sterr is 
   #required
-  (verilator --lint-only  $include_dirs "$p"| grep 'warning\|error')2>&1 | tee -a $artifact 
+  (verilator --lint-only  $include_dirs "$p") 
+  #Detect if there was an error in previous command
+  if [ "$?" -ne "0" ]; then
+   exit 1
+  fi
 done <<< "$rtl_files"
 
-#check if there is an artifact, due to errors or warnings
-#if 0 is returned by exit no log has been generated
-#else there are errors and pipeline as failed
-#this checks if $artifacs exist
-! [ -s $artifact ]
-#Stores previous return value, inverted.
-rval="$?"
-exit $rval
+exit 0
