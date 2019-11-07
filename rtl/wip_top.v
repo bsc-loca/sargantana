37cbe4293946a670edb78d1a7a26dc15b101d0c8
Guillem <cabo.guillem@gmail.com>
2019-11-07 14:55:03 +0100

    wip add top
    only inputs connected so far

--- rtl/wip_top.v
+++ rtl/wip_top.v
@@ -1,198 +1,108 @@
-`include "definitions.v"
+//TODO: add DRAC header
+//TODO: replace definitions with package drac
+`include "definitions.vh" 
 
-module top(
-	input 					    CLK,
-	input 					    RST,
-	input                       SOFT_RST,
-	input	`ADDR			    RESET_ADDRESS,
-	//--------------------------------------------------------------------------------------------------------------------------
-	// CSR INTERFACE
-	//--------------------------------------------------------------------------------------------------------------------------
-	input    `DATA         CSR_RW_RDATA,
-	input                       CSR_CSR_REPLAY,
-	input                       CSR_CSR_STALL,
-	input                       CSR_XCPT,
-	input                       CSR_ERET,
-	input    `ADDR              CSR_EVEC,
-	input                       CSR_INTERRUPT,
-	input    `DATA         CSR_INTERRUPT_CAUSE,
-
-	output   [11:0]             CSR_RW_ADDR,
-	output   [2:0]              CSR_RW_CMD,
-	output   `DATA         CSR_RW_WDATA,
-	output                      CSR_EXCEPTION,
-	output                      CSR_RETIRE,
-	output   `DATA         CSR_CAUSE,
-	output   `ADDR              CSR_PC,
-	//--------------------------------------------------------------------------------------------------------------------------
-	// I-CACHE INTERFACE
-	//--------------------------------------------------------------------------------------------------------------------------
-	output 				        ICACHE_INVALIDATE,
-	output	[11:0]				ICACHE_REQ_BITS_IDX,
-	output					    ICACHE_REQ_BITS_KILL,
-	output                      ICACHE_REQ_VALID,
-	output	 				    ICACHE_RESP_READY,
-	output	[27:0]				TLB_REQ_BITS_VPN,
-	output					    TLB_REQ_VALID,
-
-	input 	`CBLOCK           	ICACHE_RESP_BITS_DATABLOCK,
-	input						ICACHE_RESP_VALID,
-	input						PTWINVALIDATE,
-	input						TLB_RESP_MISS,
-	input						TLB_RESP_XCPT_IF,
-	//--------------------------------------------------------------------------------------------------------------------------
-	// D-CACHE  INTERFACE
-	//--------------------------------------------------------------------------------------------------------------------------
-	output                      DMEM_REQ_VALID,  
-	output	 `DATA  		    DMEM_OP_TYPE,
-	output   [4:0]              DMEM_REQ_CMD,
-	output   `DATA         DMEM_REQ_BITS_DATA,
-	output   `ADDR              DMEM_REQ_BITS_ADDR,
-	output   [7:0]              DMEM_REQ_BITS_TAG,
-	output                      DMEM_REQ_INVALIDATE_LR,
-	output                      DMEM_REQ_BITS_KILL,
-
-	input                       DMEM_ORDERED,
-	input                       DMEM_REPLAY_NEXT_VALID,
-	input                       DMEM_REQ_READY,
-	input    `DATA         DMEM_RESP_BITS_DATA,
-	input    `DATA         DMEM_RESP_BITS_DATA_SUBW,
-	input                       DMEM_RESP_BITS_HAS_DATA,
-	input                       DMEM_RESP_BITS_NACK,
-	input                       DMEM_RESP_BITS_REPLAY,
-	input    [7:0]              DMEM_RESP_BITS_TAG,
-	input                       DMEM_RESP_VALID,
-	input                       DMEM_XCPT_MA_ST,
-	input                       DMEM_XCPT_MA_LD,
-	input                       DMEM_XCPT_PF_ST,
-	input                       DMEM_XCPT_PF_LD,
-	//--------------------------------------------------------------------------------------------------------------------------
-	// DEBUGGING MODULE SIGNALS
-	//--------------------------------------------------------------------------------------------------------------------------
-	input                       istall_test,
-	// PC
-	output `ADDR                IO_FETCH_PC,
-	output `ADDR                IO_DEC_PC,
-	output `ADDR                IO_RR_PC,
-	output `ADDR                IO_EXE_PC,
-	output `ADDR                IO_WB_PC,
-
-	output                      IO_WB_PC_VALID,
-	output [4:0]                IO_WB_ADDR,
-	output                      IO_WB_WE,
-	output [63:0]               IO_WB_BITS_ADDR,
-
-	input `ADDR		            IO_FETCH_PC_VALUE,
-	input			            IO_FETCH_PC_UPDATE,
-
-	input                       IO_REG_READ,
-	input [4:0]                 IO_REG_ADDR,     // Address used for both read and write operations
-	output [63:0]               IO_REG_READ_DATA,
-	input                       IO_REG_WRITE,
-	input [63:0]                IO_REG_WRITE_DATA,
-	// PMU INTERFACE
-	//--------------------------------------------------------------------------------------------------------------------------
-	output                      io_core_pmu_branch_miss,
-	output                      io_core_pmu_EXE_STORE,
-	output                      io_core_pmu_EXE_LOAD,
-	//output   [39:0]             io_core_pmu_EXE_PC,
-	output                      io_core_pmu_new_instruction,
-	//--------------------------------------------------------------------------------------------------------------------------
-	// SEÑALES EXTRAS NECESARIAS PARA CONECTAR AL SOC - NO HACEN NADA, PERO SIN ELLAS EXISTE UN ERROR
-	//--------------------------------------------------------------------------------------------------------------------------
-	output io_dmem_req_bits_phys,
-	input [39:0] io_dmem_resp_bits_addr,
-	input [4:0] io_dmem_resp_bits_cmd,
-	input [3:0] io_dmem_resp_bits_typ,
-	input [63:0] io_dmem_resp_bits_store_data,
-	input [7:0] io_dmem_replay_next_bits,
-	output[31:0] io_fpu_inst,
-	output[63:0] io_fpu_fromint_data,
-	output[2:0] io_fpu_fcsr_rm,
-	input  io_fpu_fcsr_flags_valid,
-	input [4:0] io_fpu_fcsr_flags_bits,
-	input [63:0] io_fpu_store_data,
-	input [63:0] io_fpu_toint_data,
-	output io_fpu_dmem_resp_val,
-	output[2:0] io_fpu_dmem_resp_type,
-	output[4:0] io_fpu_dmem_resp_tag,
-	output[63:0] io_fpu_dmem_resp_data,
-	output io_fpu_valid,
-	input  io_fpu_fcsr_rdy,
-	input  io_fpu_nack_mem,
-	input  io_fpu_illegal_rm,
-	output io_fpu_killx,
-	output io_fpu_killm,
-	input  io_fpu_dec_wen,
-	input  io_fpu_dec_ren1,
-	input  io_fpu_dec_ren2,
-	input  io_fpu_dec_ren3,
-	input  io_fpu_sboard_set,
-	input  io_fpu_sboard_clr,
-	input [4:0] io_fpu_sboard_clra,
-	input  io_rocc_cmd_ready,
-	output io_rocc_cmd_valid,
-	output[6:0] io_rocc_cmd_bits_inst_funct,
-	output[4:0] io_rocc_cmd_bits_inst_rs2,
-	output[4:0] io_rocc_cmd_bits_inst_rs1,
-	output io_rocc_cmd_bits_inst_xd,
-	output io_rocc_cmd_bits_inst_xs1,
-	output io_rocc_cmd_bits_inst_xs2,
-	output[4:0] io_rocc_cmd_bits_inst_rd,
-	output[6:0] io_rocc_cmd_bits_inst_opcode,
-	output[63:0] io_rocc_cmd_bits_rs1,
-	output[63:0] io_rocc_cmd_bits_rs2,
-	output io_rocc_s,
-	output io_rocc_exception,
-	input [31:0] io_icache_resp_bits_data,
-	input  io_icache_mem_acquire_valid,
-	input [25:0] io_icache_mem_acquire_bits_addr_block,
-	input  io_icache_mem_acquire_bits_client_xact_id,
-	input [1:0] io_icache_mem_acquire_bits_addr_beat,
-	input [127:0] io_icache_mem_acquire_bits_data,
-	input  io_icache_mem_acquire_bits_is_builtin_type,
-	input [2:0] io_icache_mem_acquire_bits_a_type,
-	input [16:0] io_icache_mem_acquire_bits_union,
-	input  io_icache_mem_grant_ready,
-	input  io_tlb_req_ready,
-	input [19:0] io_tlb_resp_ppn,
-	input  io_tlb_resp_xcpt_ld,
-	input  io_tlb_resp_xcpt_st,
-	input [7:0] io_tlb_resp_hit_idx,
-	input  io_tlb_ptw_req_valid,
-	input [26:0] io_tlb_ptw_req_bits_addr,
-	input [1:0] io_tlb_ptw_req_bits_prv,
-	input  io_tlb_ptw_req_bits_store,
-	input  io_tlb_ptw_req_bits_fetch,
-	input  io_csr_status_sd,
-	input [30:0] io_csr_status_zero2,
-	input  io_csr_status_sd_rv32,
-	input [8:0] io_csr_status_zero1,
-	input [4:0] io_csr_status_vm,
-	input  io_csr_status_mprv,
-	input [1:0] io_csr_status_xs,
-	input [1:0] io_csr_status_fs,
-	input [1:0] io_csr_status_prv3,
-	input  io_csr_status_ie3,
-	input [1:0] io_csr_status_prv2,
-	input  io_csr_status_ie2,
-	input [1:0] io_csr_status_prv1,
-	input  io_csr_status_ie1,
-	input [1:0] io_csr_status_prv,
-	input  io_csr_status_ie,
-	input [31:0] io_csr_ptbr,
-	input  io_csr_fatc,
-	input [63:0] io_csr_time,
-	input [2:0] io_csr_fcsr_rm,
-	output io_csr_fcsr_flags_valid,
-	output[4:0] io_csr_fcsr_flags_bits,
-	input  io_csr_pcr_req_valid,
-	input  io_csr_pcr_req_bits_coreId,
-	input [11:0] io_csr_pcr_req_bits_addr,
-	input [2:0] io_csr_pcr_req_bits_cmd,
-	input [63:0] io_csr_pcr_req_bits_data,
-	output[63:0] io_counter_wb
+module top_drac(
+    //original inputs of lagarto
+    input                         CLK,
+    input                         RST,
+    input                         SOFT_RST,
+    input    `ADDR                RESET_ADDRESS,
+    //CSR inputs
+    input    `DATA              CSR_RW_RDATA,
+    input                       CSR_CSR_STALL,
+    input                       CSR_XCPT,
+    input                       CSR_ERET,
+    input    `ADDR              CSR_EVEC,
+    input                       CSR_INTERRUPT,
+    input    `DATA              CSR_INTERRUPT_CAUSE,
+    //ICache
+    input     `CBLOCK           ICACHE_RESP_BITS_DATABLOCK,
+    input                       ICACHE_RESP_VALID,
+    input                       PTWINVALIDATE,
+    input                       TLB_RESP_MISS,
+    input                       TLB_RESP_XCPT_IF,
+    //DMEM
+    input                       DMEM_ORDERED,
+    input                       DMEM_REQ_READY,
+    input    `DATA              DMEM_RESP_BITS_DATA_SUBW,
+    input                       DMEM_RESP_BITS_NACK,
+    input                       DMEM_RESP_BITS_REPLAY,
+    input                       DMEM_RESP_VALID,
+    input                       DMEM_XCPT_MA_ST,
+    input                       DMEM_XCPT_MA_LD,
+    input                       DMEM_XCPT_PF_ST,
+    input                       DMEM_XCPT_PF_LD,
+    //fetch
+    input `ADDR                 IO_FETCH_PC_VALUE,
+    input                       IO_FETCH_PC_UPDATE,
+    //debug register file?
+    input                       IO_REG_READ,
+    input [4:0]                 IO_REG_ADDR,     
+    input                       IO_REG_WRITE,
+    input [63:0]                IO_REG_WRITE_DATA
+    //TODO:add outputs later
 );
 
-endmodule
+//instance of the struct  for interface icache and cpu
+req_icache_cpu_t req_icache_cpu_i;
+//translate from SoC signals to struct
+req_icache_cpu_i.valid = ICACHE_RESP_VALID;
+req_icache_cpu_i.data =  ICACHE_RESP_BITS_DATABLOCK;
+//TODO: If not needed remove them from struct
+//req_icache_cpu_i.instr_addr_misaligned = ;
+//req_icache_cpu_i.instr_access_fault = ;
+req_icache_cpu_i.instr_page_fault = PTWINVALIDATE ;
 
+//instance of the struct for interface dcache and cpu
+req_dcache_cpu_t req_icache_cpu_i;
+//translate from SoC signals to struct
+req_icache_cpu_i.dmem_resp_replay_i = DMEM_RESP_BITS_REPLAY;
+req_icache_cpu_i.dmem_resp_data_i = DMEM_RESP_BITS_DATA_SUBW;
+req_icache_cpu_i.dmem_req_ready_i = DMEM_REQ_READY;
+req_icache_cpu_i.dmem_resp_valid_i = DMEM_RESP_VALID ;
+req_icache_cpu_i.dmem_resp_nack_i = DMEM_RESP_BITS_NACK;
+req_icache_cpu_i.dmem_xcpt_ma_st_i = DMEM_XCPT_MA_ST;
+req_icache_cpu_i.dmem_xcpt_ma_ld_i = DMEM_XCPT_MA_LD;
+req_icache_cpu_i.dmem_xcpt_pf_st_i = DMEM_XCPT_PF_ST;
+req_icache_cpu_i.dmem_xcpt_pf_ld_i = DMEM_XCPT_PF_LD;
+
+datapath datapath_inst(
+    .clk_i(CLK),
+    .rstn_i(RST),
+    .soft_rstn_i(SOFT_RST),
+    .req_icache_cpu_i(req_icache_cpu_i),//req_icache_cpu_t
+    .req_dcache_cpu_i(req_dcache_cpu_i),//req_dcache_cpu_t
+//TODO:connect outputs later
+    .req_cpu_dcache_o(), //req_cpu_dcache_t
+    .req_cpu_icache_o() //req_cpu_icache_t
+
+);
+/*
+icache_interface icache_interface_inst(
+    clk_i(CLK),
+    rstn_i(RST),
+    icache_resp_valid_i(ICACHE_RESP_VALID), // ICACHE_RESP_VALID,
+    ptw_invalidate_i(PTWINVALIDATE), // PTWINVALIDATE,
+    tlb_resp_miss_i(TLB_RESP_MISS), // TLB_RESP_MISS,
+    tlb_resp_xcp_if_i(TLB_RESP_XCPT_IF), // TLB_RESP_XCPT_IF,
+    icache_invalidate_o(), // ICACHE_INVALIDATE
+    icache_req_kill_o(), // ICACHE_REQ_BITS_KILL,
+    icache_req_valid_o(), // ICACHE_REQ_VALID,
+    icache_resp_ready_o(), // ICACHE_RESP_READY,
+    tlb_req_valid_o(), // TLB_REQ_VALID
+//TODO:
+    icache_idx_t         icache_req_bits_idx_o(), // ICACHE_REQ_BITS_IDX,
+    // Fetch stage interface - Request packet from fetch_stage
+    req_cpu_icache_t   req_fetch_icache_i(),
+    // Request  signals from ICache
+    icache_line_t      icache_resp_datablock_i(), // ICACHE_RESP_BITS_DATABLOCK
+    icache_vpn_t         tlb_req_bits_vpn_o(), // TLB_REQ_BITS_VPN,
+    // Fetch stage interface - Request packet icache to fetch
+    req_icache_cpu_t  req_icache_fetch_o
+);
+
+dcache_interface dcache_interface_inst(
+);
+*/
+endmodule
