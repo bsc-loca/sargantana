2280f61e80c05199836673d08d4e49bc71037434
Arnau Bigas <arnau.bigas@bsc.es>
2023-06-27 18:23:59 +0200

    Create signatures folder if missing in run_torture

--- tb/tb_torture/run_torture.sh
+++ tb/tb_torture/run_torture.sh
@@ -9,6 +9,10 @@ BINARY="tb/tb_torture/riscv-torture/output/test_$CONFIG.riscv"
 SIM_LOG=tb/tb_torture/signatures/$CONFIG\_sim.txt
 SPIKE_LOG=tb/tb_torture/signatures/$CONFIG\_spike.txt
 
+if ! [[ -f "tb/tb_torture/signatures" ]]; then
+    mkdir -p tb/tb_torture/signatures
+fi
+
 if [[ -f "$BINARY" ]]; then
     $SIM +load=$BINARY +torture_dump_ON +torture_dump=$SIM_LOG
     $SPIKE -l --log-commits --isa=rv64g --mmu-dirty --log=$SPIKE_LOG $BINARY
