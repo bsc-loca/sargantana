3a67bf954a2296355c361c10a78d8d933e96f64b
Juan Antonio Rodriguez Gracia <juan.rodriguez4@bsc.es>
2024-06-11 11:46:53 +0200

    fix for lmul>=1

--- rtl/datapath/rtl/id_stage/rtl/decoder.sv
+++ rtl/datapath/rtl/id_stage/rtl/decoder.sv
@@ -1247,11 +1247,19 @@ module decoder
                                         decode_instr_int.instr_type = VSSRA;
                                     end
                                     F6_VNCLIPU: begin
-                                        decode_instr_int.instr_type = VNCLIPU;
+                                        if (!v_2sew_en_i) begin
+                                            xcpt_illegal_instruction_int = 1'b1;
+                                        end else begin
+                                            decode_instr_int.instr_type = VNCLIPU;
+                                        end
                                     end
                                     F6_VNCLIP: begin
-                                        decode_instr_int.instr_type = VNCLIP;
-                                    end
+                                        if (!v_2sew_en_i) begin
+                                            xcpt_illegal_instruction_int = 1'b1;
+                                        end else begin
+                                            decode_instr_int.instr_type = VNCLIP;
+                                        end
+                                    end                                    
                                     F6_VNSRL: begin
                                         if ((!v_2sew_en_int) || (decode_instr_int.vs2[0] && (~vlmul_int[2]))) begin
                                             xcpt_illegal_instruction_int = 1'b1;
@@ -1362,17 +1370,25 @@ module decoder
                                         decode_instr_int.instr_type = VSSRA;
                                     end
                                     F6_VNCLIPU: begin
-                                        if (decode_instr_int.imm > ((2**5)-1)) begin
+                                       if (!v_2sew_en_i) begin
                                             xcpt_illegal_instruction_int = 1'b1;
                                         end else begin
-                                            decode_instr_int.instr_type = VNCLIPU;
+                                            if (decode_instr_int.imm > ((2**5)-1)) begin
+                                                xcpt_illegal_instruction_int = 1'b1;
+                                            end else begin
+                                                decode_instr_int.instr_type = VNCLIPU;
+                                            end
                                         end
                                     end
                                     F6_VNCLIP: begin
-                                        if (decode_instr_int.imm > ((2**5)-1)) begin
+                                        if (!v_2sew_en_i) begin
                                             xcpt_illegal_instruction_int = 1'b1;
                                         end else begin
-                                            decode_instr_int.instr_type = VNCLIP;
+                                            if (decode_instr_int.imm > ((2**5)-1)) begin
+                                                xcpt_illegal_instruction_int = 1'b1;
+                                            end else begin
+                                                decode_instr_int.instr_type = VNCLIP;
+                                            end
                                         end
                                     end                                    
                                     F6_VNSRL: begin
